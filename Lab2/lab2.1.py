3c749c7303210d5c9ad9e3dd9d7d7c111570ad76
Олико Человик <kaper156oi@gmail.com>
2019-10-10 00:19:09 +0600

    Cipher Caesar (2.1) Add only functional cipher-method

--- Lab2/lab2.1.py
+++ Lab2/lab2.1.py
@@ -15,7 +15,20 @@ def caesar_cipher(letter: str, key: int):
     return chr((ord(letter) - register(letter) + key) % ABC_LENGTH + register(letter))
 
 
+def in_one_string():
+    key = int(input("Enter key:"))
+    L = ABC_LENGTH
+    A = ABC_UPPER_FIRST
+    a = ABC_LOWER_FIRST
+    return "".join(map(
+        lambda x: chr(
+            (ord(x) + key - (ord(a) if x.islower() else ord(A))) % L
+            + (ord(a) if x.islower() else ord(A))) if x.isalpha() else x,
+        open(FILE_PATH_INPUT, 'rt').read()))
+
+
 if __name__ == '__main__':
+    print(in_one_string())
     with open(FILE_PATH_INPUT, 'rt') as f_in:
         text = f_in.read()
     key = int(input("Enter key:"))
