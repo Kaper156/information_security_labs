1a4714e42f9e00e922cca4b8ee2f51f311d0480b
Олико Человик <kaper156oi@gmail.com>
2019-10-10 04:18:29 +0600

    Lab2_2: do one method with two lambda-functions for cipher and decipher

--- Lab2/lab2_2.py
+++ Lab2/lab2_2.py
@@ -2,45 +2,52 @@ from Lab2.lab2_1 import register
 from Lab2.lab2_settings import *
 
 
-def affine_cipher(letter, A, B, decipher=False):
-    if not letter.isalpha():
-        return letter
+def affine(text, A, B, is_decipher=False):
+    result = str()
+    if not is_decipher:
+        transformation = lambda code: (A * code + B) % ABC_LENGTH
+    else:
+        transformation = lambda code: (A * (code - B)) % ABC_LENGTH
+    for letter in text:
+        if not letter.isalpha():
+            result += letter
+            continue
 
-    return chr(((ord(letter) - register(letter)) * A + B*(+1 if not decipher else -1)) % ABC_LENGTH + register(letter))
-
-
-def in_one_string(text=None, ka=None, kb=None, decipher=False):
-    key_a = ka or int(input("Enter key:"))
-    key_b = kb or int(input("Enter key:"))
-    L = ABC_LENGTH
-    A = ABC_UPPER_FIRST
-    a = ABC_LOWER_FIRST
-    return "".join(map(
-        lambda x: chr(
-            ((ord(x) - (ord(a) if x.islower() else ord(A))) * key_a
-                + key_b*(+1 if not decipher else -1)) % L
-            + (ord(a) if x.islower() else ord(A))) if x.isalpha() else x,
-        text or open(FILE_PATH_INPUT, 'rt').read()))
+        offset = register(letter)
+        code = ord(letter) - offset
+        result += chr(transformation(code) + offset)
+    return result
 
 
 def test():
     key_a = 3
     key_a_inv = 9
-    key_b = ABC_LENGTH + 4
-    text = "Some text from abc"
-    # text = "ATTACK AT DAWN"
+    key_b = 4
+    text = "ATTACK AT DAWN"
 
     # ABC length is not important
-    assert in_one_string(text, key_a, key_b) == in_one_string(text, key_a, key_b - ABC_LENGTH)
+    assert affine(text, key_a, key_b) == affine(text, key_a, key_b - ABC_LENGTH)
 
     # Can be deciphered
-    ciphered_text = in_one_string(text, key_a, key_b, False)
-    assert in_one_string(ciphered_text, key_a_inv, key_b, True) == text
+    ciphered_text = affine(text, key_a, key_b, False)
+    assert affine(ciphered_text, key_a_inv, key_b, True) == text
 
     # Simple test
     text = "ATTACK AT DAWN"
-    crypto = in_one_string(text, key_a_inv, key_b)
-    assert crypto == "EJJEKI EJ NESR"
+    ciphered_text = affine(text, key_a, key_b)
+    assert ciphered_text == "EJJEKI EJ NESR"
+
+    # Big file with lorem
+    # Cipher:
+    with open(FILE_PATH_INPUT, 'rt') as f_in:
+        text = f_in.read()
+
+    ciphered_text = affine(text, key_a, key_b)
+    # Text is ciphered
+    assert text != ciphered_text
+
+    # All text is deciphered
+    assert affine(ciphered_text, key_a_inv, key_b, True) == text
 
 
 if __name__ == '__main__':
@@ -49,7 +56,7 @@ if __name__ == '__main__':
         text = f_in.read()
     key_a = int(input("Enter key A:"))
     key_b = int(input("Enter key B:"))
-    cipher = "".join(map(lambda x: affine_cipher(x, key_a, key_b), text))
+    cipher = "".join(map(lambda x: affine(x, key_a, key_b), text))
     with open(FILE_PATH_OUT, 'wt') as f:
         f.write(cipher)
     print(cipher)
