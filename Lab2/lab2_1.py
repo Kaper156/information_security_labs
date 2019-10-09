1d3560be061be10ca343446aaf496dece7407d1f
Олико Человик <kaper156oi@gmail.com>
2019-10-10 01:18:57 +0600

    Add tests for lab2_1

--- Lab2/lab2_1.py
+++ Lab2/lab2_1.py
@@ -11,8 +11,8 @@ def caesar_cipher(letter: str, key: int):
     return chr((ord(letter) - register(letter) + key) % ABC_LENGTH + register(letter))
 
 
-def in_one_string():
-    key = int(input("Enter key:"))
+def in_one_string(text=None, k=None):
+    key = k or int(input("Enter key:"))
     L = ABC_LENGTH
     A = ABC_UPPER_FIRST
     a = ABC_LOWER_FIRST
@@ -20,11 +20,29 @@ def in_one_string():
         lambda x: chr(
             (ord(x) + key - (ord(a) if x.islower() else ord(A))) % L
             + (ord(a) if x.islower() else ord(A))) if x.isalpha() else x,
-        open(FILE_PATH_INPUT, 'rt').read()))
+        text or open(FILE_PATH_INPUT, 'rt').read()))
+
+
+def test():
+    key = ABC_LENGTH + 5
+    text = "Some text from abc"
+
+    # ABC length is not important
+    assert in_one_string(text, key) == in_one_string(text, key - ABC_LENGTH)
+
+    # Can be deciphered
+    ciphered_text = in_one_string(text, key)
+    assert in_one_string(ciphered_text, -key) == text
+
+    # Simple test
+    text = "ABC"
+    key = 3
+    crypto = in_one_string(text, key)
+    assert crypto == "DEF"
 
 
 if __name__ == '__main__':
-    print(in_one_string())
+    test()
     with open(FILE_PATH_INPUT, 'rt') as f_in:
         text = f_in.read()
     key = int(input("Enter key:"))
